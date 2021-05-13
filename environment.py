f335f64b2f6cf50b3fb0e8dbe9619cfe98074f4f
mschoder <schoder.m@gmail.com>
2021-05-13 14:56:39 -0700

    env & spline gen

---
+++ environment.py
@@ -0,0 +1,114 @@
+import math
+import numpy as np
+import shapely.geometry as geom
+from shapely import affinity
+import itertools
+from matplotlib import pyplot as plt
+from descartes import PolygonPatch
+
+from trajectory_gen import sample_trajectory
+import scenarios
+
+class Environment:
+    def __init__(self, obstacles, start, goal_region, bounds=None):
+        self.environment_loaded = False
+        self.obstacles = obstacles  # list of lists of tuples
+        self.bounds = bounds
+        self.start = start  # (x,y)
+        self.goal_region = goal_region  # list of tuples defining corner points
+        self.control_points = []
+        self.calculate_scene_dimensions()
+
+    def add_obstacles(self, obstacles):
+        self.obstacles += obstacles
+        self.calculate_scene_dimensions()
+
+    def set_goal_region(self, goal_region):
+        self.goal_region = goal_region
+        self.calculate_scene_dimensions()
+
+    def add_control_points(self, points):
+        self.control_points += points
+        self.calculate_scene_dimensions()
+
+    def calculate_scene_dimensions(self):
+        """Compute scene bounds from obstacles, start, and goal """
+        points = []
+        for elem in self.obstacles:
+            points = points + elem
+        if self.start:
+            points += [self.start]
+        if self.goal_region:
+            points += self.goal_region
+        if len(self.control_points) > 0:
+            points += self.control_points
+        mp = geom.MultiPoint(points)
+        self.bounds = mp.bounds
+
+
+def plot_environment(env, bounds=None, figsize=None, margin=1.0):
+    if bounds is None and env.bounds:
+        minx, miny, maxx, maxy = env.bounds
+        minx -= margin
+        miny -= margin
+        maxx += margin
+        maxy += margin
+    elif bounds:
+        minx, miny, maxx, maxy = bounds
+    else:
+        minx, miny, maxx, maxy = (-10, -5, 10, 5)
+    max_width, max_height = 12, 5.5
+    if figsize is None:
+        width, height = max_width, (maxy-miny)*max_width/(maxx-minx)
+        if height > 5:
+            width, height = (maxx-minx)*max_height/(maxy-miny), max_height
+        figsize = (width, height)
+
+    f = plt.figure(figsize=figsize)
+    ax = f.add_subplot(111)
+    # obstacles
+    for i, obs in enumerate(env.obstacles):
+        poly = geom.Polygon(obs)
+        patch = PolygonPatch(poly, fc='orange', ec='black', alpha=0.5, zorder=20)
+        ax.add_patch(patch)
+
+    # start / goal
+    goal_poly = geom.Polygon(env.goal_region)
+    ax.add_patch(PolygonPatch(goal_poly, fc='green',
+                 ec='green', alpha=0.5, zorder=1))
+    start = geom.Point(env.start).buffer(0.2, resolution=3)
+    ax.add_patch(PolygonPatch(start, fc='red',
+                 ec='black', alpha=0.7, zorder=1))
+
+    # control points
+    for pt in env.control_points:
+        ax.add_patch(PolygonPatch(geom.Point(pt).buffer(
+            0.1, resolution=3), fc='black', ec='black', alpha=1, zorder=1))
+
+    plt.xlim([minx, maxx])
+    plt.ylim([miny, maxy])
+    ax.set_aspect('equal', adjustable='box')
+    return ax
+
+
+# Test
+if __name__ == '__main__':
+    
+    scene = scenarios.two_obstacle
+    control_pts = [(3, 0), (4.5, 1), (5, 2.8),
+                   (3.5, 3.4), (2, 5.1), (3.7, 6.7), (5.5, 6.3)]
+    env = Environment(scene['obs_list'],
+                      scene['start'], scene['goal'])
+    env.add_control_points(control_pts)
+    ax = plot_environment(env)
+
+    xc = [p[0] for p in control_pts]
+    yc = [p[1] for p in control_pts]
+    v = 5
+    dt = 0.1
+    bc_headings = (np.pi/8, -np.pi/12)
+    xs, ys = sample_trajectory(xc, yc, bc_headings, v, dt)
+    ax.plot(xs, ys, 'ob', alpha=0.8, markersize=4, color='darkblue')
+   
+
+    plt.show()
---
+++ trajectory_gen.py
@@ -0,0 +1,223 @@
+import numpy as np
+import math
+from scipy.interpolate import CubicSpline
+import matplotlib.pyplot as plt
+
+
+''' TODO
+- compute velocity profile
+- sample along fitted spline every dt
+'''
+
+import numpy as np
+import math
+from scipy.interpolate import CubicSpline
+import matplotlib.pyplot as plt
+
+
+def sample_trajectory(x, y, bc_headings, v, dt):
+    ''' Given x,y, control points, boundary condition headings, fixed velocity v,
+        return a sampled C2 trajectory every time period dt
+    '''
+    
+    cx, cy = calc_c2_traj(x, y, bc_headings)
+
+    total_length = 0
+    for i in range(cx.c.shape[1]):
+        coeffs_x = np.flip(cx.c[:, i])
+        coeffs_y = np.flip(cy.c[:, i])
+        slen = calc_spline_length(coeffs_x, coeffs_y)
+        print(slen)
+        total_length += slen
+
+    print('total len: ', total_length)
+    nsteps = int(total_length/(dt*v))
+    # tvec = np.arange(0, len(x)-1+dt, dt)
+    tvec = np.linspace(0, len(x)-1, nsteps)
+    xs = cx(tvec)
+    ys = cy(tvec)
+    return xs, ys
+
+
+def calc_c2_traj(x, y, bc_headings, eps=0.005):
+    '''
+    Iteratively compute spline coefficients until spline length of first and last segment converges
+    '''
+
+    # Start with euclidean dist as slen approx for first and last segments
+    slen_start = np.sqrt((x[1] - x[0])**2 + (y[1] - y[0])**2)
+    slen_end = np.sqrt((x[-1] - x[-2])**2 + (y[-1] - y[-2])**2)
+
+    while True:
+        cx, cy = gen_c2_spline(x, y, bc_headings, slen_start, slen_end)
+        coeffs_x_start = np.flip(cx.c[:, 0])
+        coeffs_y_start = np.flip(cy.c[:, 0])
+        coeffs_x_end = np.flip(cx.c[:, -1])
+        coeffs_y_end = np.flip(cy.c[:, -1])
+
+        slen_start_new = calc_spline_length(coeffs_x_start, coeffs_y_start)
+        slen_end_new = calc_spline_length(coeffs_x_end, coeffs_y_end)
+#         print(slen_start_new, slen_end_new)
+
+        if abs(slen_start_new - slen_start) < eps and abs(slen_end_new - slen_end) < eps:
+            break
+        else:
+            slen_start = slen_start_new
+            slen_end = slen_end_new
+    return cx, cy
+
+
+def gen_c2_spline(x, y, bc_headings, slen_start, slen_end):
+    '''
+    Generates a C2 continuous spline using scipy CubicSpline lib
+    x: np.array of x-coordinate points
+    y: np.array of y-coordinate points
+    '''
+
+    # define mu, a virtual path variable of length 1 for each spline segment
+    assert(len(x) == len(y))
+    mu = np.arange(0, len(x), 1.0)
+
+    # build splines
+    cs_x = CubicSpline(mu, x,
+                       bc_type=((1, slen_start * np.cos(bc_headings[0])),
+                                (1, slen_end * np.cos(bc_headings[1]))))
+    cs_y = CubicSpline(mu, y,
+                       bc_type=((1, slen_start * np.sin(bc_headings[0])),
+                                (1, slen_end * np.sin(bc_headings[1]))))
+    return cs_x, cs_y
+
+
+def unitvec_from_heading(theta):
+    x = np.cos(theta)
+    y = np.sin(theta)
+    ds = (x**2 + y**2)**0.5
+    return (x/ds, y/ds)
+
+
+def calc_spline_length(x_coeffs, y_coeffs, n_ips=20):
+    '''
+    Returns numerically computed length along cubic spline
+    x_coeffs: array of 4 x coefficients
+    y_coeffs: array of 4 y coefficients
+    '''
+
+    t_steps = np.linspace(0.0, 1.0, n_ips)
+    spl_coords = np.zeros((n_ips, 2))
+
+    spl_coords[:, 0] = x_coeffs[0] \
+        + x_coeffs[1] * t_steps \
+        + x_coeffs[2] * np.power(t_steps, 2) \
+        + x_coeffs[3] * np.power(t_steps, 3)
+    spl_coords[:, 1] = y_coeffs[0] \
+        + y_coeffs[1] * t_steps \
+        + y_coeffs[2] * np.power(t_steps, 2) \
+        + y_coeffs[3] * np.power(t_steps, 3)
+
+    slength = np.sum(
+        np.sqrt(np.sum(np.power(np.diff(spl_coords, axis=0), 2), axis=1)))
+    return slength
+
+
+def plot_trajectory(x, y, bch, cx, cy, stepsize=0.1):
+    '''
+    Plots x-y coords and cx(t)-cy(t) parametric spline
+    Plots unit vectors showing the spline headings at boundaries
+    Generates c1 and c2 plots showing heading and curvature continuity
+    '''
+
+    ts = np.arange(0, len(x)-1+stepsize, stepsize)
+    ts_plus = np.arange(ts[0]-.2, ts[-1]+.3, stepsize)
+
+    # Heading constraint unit vectors
+    hvec_start = unitvec_from_heading(bch[0])
+    hvec_end = unitvec_from_heading(bch[-1])
+
+    # Plot trajectory
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.set_ylim(min(y)-1, max(y)+1)
+    ax.set_xlim(min(x)-2, max(x)+2)
+    ax.plot(x, y, 'o', label='nodes')
+    ax.plot(cx(ts_plus), cy(ts_plus), label='spline')
+    ax.annotate("", xy=(x[0] + hvec_start[0], y[0] + hvec_start[1]),
+                xytext=(x[0], y[0]), arrowprops=dict(arrowstyle="->", color="red"))
+    ax.annotate("", xy=(x[-1] + hvec_end[0], y[-1] + hvec_end[1]),
+                xytext=(x[-1], y[-1]), arrowprops=dict(arrowstyle="->", color="red"))
+    ax.set_aspect('equal')
+    ax.set_title('C2 trajectory')
+    ax.set_xlabel('x(mu)')
+    ax.set_ylabel('y(mu)')
+
+    # Plot heading and curvature
+    fig, ax = plt.subplots(1, 2, figsize=(14, 4))
+    ax[0].set_title('X(mu)')
+    ax[0].plot(ts, cx(ts, 1), label='Heading')
+    ax[0].plot(ts, cx(ts, 2), label='Curvature')
+    ax[0].set_xlabel('mu')
+    ax[0].legend()
+    ax[1].set_title('Y(mu)')
+    ax[1].plot(ts, cy(ts, 1), label='Heading')
+    ax[1].plot(ts, cy(ts, 2), label='Curvature')
+    ax[1].set_xlabel('mu')
+    ax[1].legend()
+
+
+## Test Case ##
+if __name__ == "__main__":
+
+    # points defined as (x,y,theta)
+    bc_headings = (np.pi/8, -np.pi/12)
+    nodes = [(0, 0, bc_headings[0]),
+             (2.0, 0.3, None),
+             (3.0, 0.5, None),
+             (4.1, -0.2, None),
+             (5.0, -0.4, None),
+             (6.0, 0.0, bc_headings[1])]
+    x = np.array([i[0] for i in nodes])
+    y = np.array([i[1] for i in nodes])
+    bch = np.array([i[2] for i in nodes])
+    theta = np.array([i[2] for i in nodes])
+
+    # Heading constraint unit vectors
+    hvec_start = unitvec_from_heading(bch[0])
+    hvec_end = unitvec_from_heading(bch[-1])
+
+    # Plot test case
+    # fig = plt.figure(figsize=(10, 5))
+    # ax = fig.add_subplot(111)
+    # ax.set_ylim(-1.5, 1.5)
+    # ax.set_xlim(-1, 7.5)
+    # ax.scatter(x, y)
+    # ax.annotate("", xy=(x[0] + hvec_start[0], y[0] + hvec_start[1]),
+    #             xytext=(x[0], y[0]), arrowprops=dict(arrowstyle="->", color="red"))
+    # ax.annotate("", xy=(x[-1] + hvec_end[0], y[-1] + hvec_end[1]),
+    #             xytext=(x[-1], y[-1]), arrowprops=dict(arrowstyle="->", color="red"))
+    # ax.set_aspect('equal')
+
+    # Test iterative calc trajectory generation
+    cx, cy = calc_c2_traj(x, y, bc_headings)
+
+    # test sampling
+    v = 2.0
+    dt = 0.1
+    xs, ys = sample_trajectory(x, y, bc_headings, v, dt)
+    plt.figure()
+    plt.plot(xs, ys, 'o')
+    plt.plot(x[0], y[0], 'ko')
+    plt.plot(x[-1], y[-1], 'ko')
+    plt.axis('equal')
+    plt.show()
+
+    # Plot
+    plot_trajectory(x, y, bch, cx, cy)
+    # plt.show()
+
+
+    # ## Test single spline segment case
+    # xr = np.array([0,2])
+    # yr = np.array([1,4])
+    # bcr = np.array([0, np.pi/8])
+    # cxr, cyr = calc_c2_traj(xr, yr, bcr)
+
+    # ## Plot
+    # plot_trajectory(xr, yr, bcr, cxr, cyr)
